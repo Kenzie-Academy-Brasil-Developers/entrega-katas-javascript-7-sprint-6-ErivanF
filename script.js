3e010729580bc055735b4738b533262aedc32fb2
Erivan Ferreira <erivan.c39@gmail.com>
2021-10-21 09:27:46 -0300

    concat corrigido

--- script.js
+++ script.js
@@ -86,9 +86,17 @@ function newIndexOf(arr,element,start=0){
     return -1
 }
 function newConcat(...arr){
-    // const result = [...arr]
-    //não sei se isso pode dar algum problema
-    return arr
+    const result =[]
+    for(let i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            for(let j=0;j<arr[i].length;j++){
+                result.push(arr[i][j])
+            }
+        }else{
+            result.push(arr[i])
+        }
+    }
+    return result
 }
 function newJoin(arr, between=","){
     let result =""
